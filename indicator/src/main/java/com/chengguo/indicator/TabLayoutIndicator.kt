7dcb1c42b317c94f5910f71e20912048738f0c71
cheng <chengguo4930@hotmail.com>
2018-12-25 00:43:22 +0800

    将kontlin项目发布到jcenter

--- indicator/src/main/java/com/chengguo/indicator/TabLayoutIndicator.kt
+++ indicator/src/main/java/com/chengguo/indicator/TabLayoutIndicator.kt
@@ -10,7 +10,6 @@ import android.graphics.drawable.Drawable
 import android.support.v4.view.ViewPager
 import android.util.AttributeSet
 import android.view.View
-import com.chengguo.zhenai.indicator.R
 
 
 /**
